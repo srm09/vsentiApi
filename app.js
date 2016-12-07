ce7fb314e6c4a2f243fcad4dfeb777f4ed524601
Sagar Muchhal <muchhalsagar88@gmail.com>
2016-12-07 15:32:15 -0800

    Exposed basic APIs

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var express = require('express')
 var app = express(),
     path = require('path')
-var mysql = require('mysql')
+var helper = require('./helper.js')
 
 // Defaults
 var productCounter = 1;
-var vProduct = function(name){
-  this.name = name
-  this.id = productCounter++;
-}
+// var vProduct = function(name){
+//   this.name = name
+//   this.id = productCounter++;
+// }
+
+// Query Strings
+GET_PRODUCT_LIST = "SELECT * FROM product"
 
 // Use middleware for hosting static files
 app.use('/public', express.static('ProjectIpsum'))
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'))
+  //res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(3000, function () {
@@ -23,7 +26,10 @@ app.listen(3000, function () {
 
 // List of products supported by vSenti analyzer
 app.get('/products', (req, res) => {
-
+  helper.executeQuery(GET_PRODUCT_LIST, (results, fields) => {
+    console.log("Fetched reults are: "+results)
+    console.log("Fetched fields are: "+fields)
+  })
 })
 
 // Time series data for product (@param product) for the lat 3 months
--- helper.js
+++ helper.js
@@ -1,3 +1,23 @@
 // Convert timestamp in millis to date object
 
 // CompareTo() timestamps
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'vsenti',
+  password : '123456',
+  database : 'vsenti_database'
+});
+
+function executeQuery(queryString, processResults) {
+  connection.connect();
+  connection.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+    console.log('Processing results for queryString: '+queryString);
+    processResults(rows, fields);
+  });
+  connection.end();
+}
+
+module.exports.executeQuery = executeQuery;
