46c8261531bc02bf02db5b370a57c08b2db695ce
Sagar Muchhal <muchhalsagar88@gmail.com>
2016-12-07 17:20:38 -0800

    Products API exposed

--- app.js
+++ app.js
@@ -20,15 +20,24 @@ app.get('/', function (req, res) {
   //res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.listen(3000, function () {
+app.listen(4000, function () {
   console.log('Example app listening on port 3000!')
 })
 
 // List of products supported by vSenti analyzer
 app.get('/products', (req, res) => {
   helper.executeQuery(GET_PRODUCT_LIST, (results, fields) => {
-    console.log("Fetched reults are: "+results)
-    console.log("Fetched fields are: "+fields)
+    console.log("Fetched reults are: "+JSON.stringify(results, null, 4))
+    console.log("Fetched fields are: "+JSON.stringify(fields, null, 4))
+    var products = []
+    for(var i=0;i <results.length; ++i) {
+      var product = results[i]
+      products.push({
+        name: product.name,
+        id: product.id
+      })
+    }
+    res.send(products)
   })
 })
 
